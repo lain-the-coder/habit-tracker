446072f25930d92c09a84217328eef91810ff2cd
Ehab Ahmed <ahmed.ehabj@gmail.com>
2025-06-05 17:42:52 +0400

    Added PUT method

--- mainapp.py
+++ mainapp.py
@@ -63,9 +63,6 @@ def add_new_habit():
     status = data.get('status')
     note = data.get('note', '')
 
-    conn = sqlite3.connect('habits.db')
-    cursor = conn.cursor()
-
     # Validations
     if not name:
         return jsonify({'error': 'Title is required'}), 400
@@ -78,6 +75,8 @@ def add_new_habit():
     if status not in ['done', 'missed']:
         return jsonify({'error': 'Status is not expected value'}), 400
     
+    conn = sqlite3.connect('habits.db')
+    cursor = conn.cursor()
     cursor.execute(
       'INSERT INTO habits (name, date, status, note) VALUES (?, ?, ?, ?)',
         (name, date, status, note)
@@ -94,6 +93,53 @@ def add_new_habit():
         'note': note
     }
     return jsonify(new_habit), 201
+
+@app.route('/habits/<int:habit_id>', methods=['PUT'])
+def update_habit(habit_id):
+    data = request.get_json()
+    name = data.get('name')
+    date = data.get('date')
+    status = data.get('status')
+    note = data.get('note', '')
+
+    # Validation checks
+    if not name:
+        return jsonify({'error': 'Name is required'}), 400
+    if not date:
+        return jsonify({'error': 'Date is required'}), 400
+    if not status:
+        return jsonify({'error': 'Status is required'}), 400
+    if not is_valid_date(date):
+        return jsonify({'error': 'Date format is wrong'}), 400
+    if status not in ['done', 'missed']:
+        return jsonify({'error': 'Status is not expected value'}), 400
+    
+    conn = sqlite3.connect('habits.db')
+    cursor = conn.cursor()
+
+    cursor.execute('SELECT * FROM habits WHERE id = ?', (habit_id,))
+    habit = cursor.fetchone()
+    if habit is None:
+        conn.close()
+        return jsonify({'error':'Expected habit for the specified id does not exist'}), 404
+    
+    cursor.execute('''
+    UPDATE habits
+    SET name = ?, date = ?, status = ?, note = ?
+    WHERE id = ?
+    ''', (name, date, status, note, habit_id))
+
+    conn.commit()
+    conn.close()
+
+    updated_habit = {
+        'id': habit_id,
+        'name': name,
+        'date': date,
+        'status': status,
+        'note': note
+    }
+    return jsonify(updated_habit), 200
 
 if __name__ == "__main__":
     app.run(debug = True)
